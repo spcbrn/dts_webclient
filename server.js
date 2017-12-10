421d49aeb9db75635cb3c733f1af3e798ba3db79
Chris Lemke <spcbrn1@gmail.com>
2017-12-10 13:57:34 -0700

    added webpack

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config;
+require('dotenv').config();
 
 const express = require('express')
     , path = require('path');
@@ -7,7 +7,7 @@ const app = express()
     , port = process.env.PORT;
 
 app.set('port', port);
-app.use(express.static(`${__dirname,}/dist`));
+app.use(express.static(`${__dirname}/dist`));
 
 
 app.get('*', (req, res) => {
